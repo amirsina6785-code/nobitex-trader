70d68655df30998d61e3dffed5517233cd9a7f8e
amirsina6785-code <amirsina6785@gmail.com>
2026-09-07 03:21:47 +0330

    Update MainActivity.kt

--- app/src/main/java/com/nobitex/trader/MainActivity.kt
+++ app/src/main/java/com/nobitex/trader/MainActivity.kt
@@ -3,44 +3,21 @@ package com.nobitex.trader
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.lifecycle.ViewModelProvider
-import com.nobitex.trader.data.TradingRepository
-import com.nobitex.trader.data.api.ApiClient
+import androidx.activity.viewModels
+import com.nobitex.trader.ui.App
 import com.nobitex.trader.ui.TradingViewModel
-import com.nobitex.trader.ui.AppNavigation
 import com.nobitex.trader.ui.theme.NobitexTraderTheme
 
 class MainActivity : ComponentActivity() {
 
+    private val viewModel: TradingViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val repository = TradingRepository(
-            apiProvider = {
-                if (!ApiClient.isInitialized()) {
-                    throw IllegalStateException(
-                        "API is not initialized. Connect to the server first."
-                    )
-                }
-                ApiClient.getApi()
-            }
-        )
-
-        val viewModel = ViewModelProvider(
-            this,
-            object : ViewModelProvider.Factory {
-                @Suppress("UNCHECKED_CAST")
-                override fun <T : androidx.lifecycle.ViewModel> create(
-                    modelClass: Class<T>
-                ): T {
-                    return TradingViewModel(repository) as T
-                }
-            }
-        )[TradingViewModel::class.java]
-
         setContent {
             NobitexTraderTheme {
-                AppNavigation(viewModel)
+                App(viewModel)
             }
         }
     }
